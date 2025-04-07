43338aed5632219b2241f6b6764507d973487e38
Morgan Pretty <mpretty-cyro@users.noreply.github.com>
2025-04-07 10:00:11 +1000

    Updated to libSession 1.3.0 (and retyping changes for wrapper code) (#4)
    * Updated to libSession 1.3.0 (and retyping changes for wrapper code)
    
    * Update library/src/main/cpp/util.cpp
    
    Co-authored-by: SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
    
    * Fixed up the build errors
    
    ---------
    
    Co-authored-by: SessionHero01 <180888785+SessionHero01@users.noreply.github.com>

--- library/src/main/cpp/blinded_key.cpp
+++ library/src/main/cpp/blinded_key.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <session/blinding.hpp>
+#include <session/util.hpp>
 
 #include "util.h"
 #include "jni_utils.h"
@@ -14,11 +15,11 @@ Java_network_loki_messenger_libsession_1util_util_BlindKeyAPI_blindVersionKeyPai
                                                                                   jobject thiz,
                                                                                   jbyteArray ed25519_secret_key) {
     return jni_utils::run_catching_cxx_exception_or_throws<jobject>(env, [=] {
-        const auto [pk, sk] = session::blind_version_key_pair(util::ustring_from_bytes(env, ed25519_secret_key));
+        const auto [pk, sk] = session::blind_version_key_pair(util::vector_from_bytes(env, ed25519_secret_key));
 
         jclass kp_class = env->FindClass("network/loki/messenger/libsession_util/util/KeyPair");
         jmethodID kp_constructor = env->GetMethodID(kp_class, "<init>", "([B[B)V");
-        return env->NewObject(kp_class, kp_constructor, util::bytes_from_ustring(env, {pk.data(), pk.size()}), util::bytes_from_ustring(env, {sk.data(), sk.size()}));
+        return env->NewObject(kp_class, kp_constructor, util::bytes_from_vector(env, {pk.data(), pk.data() + pk.size()}), util::bytes_from_vector(env, {sk.data(), sk.data() + sk.size()}));
     });
 }
 extern "C"
@@ -28,8 +29,8 @@ Java_network_loki_messenger_libsession_1util_util_BlindKeyAPI_blindVersionSign(J
                                                                                jbyteArray ed25519_secret_key,
                                                                                jlong timestamp) {
     return jni_utils::run_catching_cxx_exception_or_throws<jbyteArray>(env, [=] {
-        auto bytes = session::blind_version_sign(util::ustring_from_bytes(env, ed25519_secret_key), session::Platform::android, timestamp);
-        return util::bytes_from_ustring(env, bytes);
+        auto bytes = session::blind_version_sign(util::vector_from_bytes(env, ed25519_secret_key), session::Platform::android, timestamp);
+        return util::bytes_from_vector(env, bytes);
     });
 }
 
@@ -45,14 +46,16 @@ Java_network_loki_messenger_libsession_1util_util_BlindKeyAPI_blindVersionSignRe
     return jni_utils::run_catching_cxx_exception_or_throws<jbyteArray>(env, [=] {
         auto methodC = util::string_from_jstring(env, method);
         auto pathC = util::string_from_jstring(env, path);
+        auto keyBytes = util::vector_from_bytes(env, ed25519_secret_key);
+        auto bodyBytes = body ? std::optional(util::vector_from_bytes(env, body)) : std::nullopt;
 
         auto bytes = session::blind_version_sign_request(
-                util::ustring_from_bytes(env, ed25519_secret_key),
+                session::to_span(keyBytes),
                 timestamp,
                 methodC,
                 pathC,
-                body ? std::optional(util::ustring_from_bytes(env, body)) : std::nullopt
+                body ? std::optional(session::to_span(*bodyBytes)) : std::nullopt
         );
-        return util::bytes_from_ustring(env, bytes);
+        return util::bytes_from_vector(env, bytes);
     });
 }
