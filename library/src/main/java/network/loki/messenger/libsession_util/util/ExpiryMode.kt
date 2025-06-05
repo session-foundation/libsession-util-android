d49511bba9f40ad468f6acc8cc9337a548ce70a1
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-06-05 16:06:52 +1000

    Use data object for non-expiry mode (#12)

--- library/src/main/java/network/loki/messenger/libsession_util/util/ExpiryMode.kt
+++ library/src/main/java/network/loki/messenger/libsession_util/util/ExpiryMode.kt
@@ -3,7 +3,7 @@ package network.loki.messenger.libsession_util.util
 import kotlin.time.Duration.Companion.seconds
 
 sealed class ExpiryMode(val expirySeconds: Long) {
-    object NONE: ExpiryMode(0)
+    data object NONE: ExpiryMode(0)
     data class AfterSend(private val seconds: Long = 0L): ExpiryMode(seconds)
     data class AfterRead(private val seconds: Long = 0L): ExpiryMode(seconds)
 
