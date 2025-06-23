55caa0f85c6b4998433080f91bbd477d0438ff26
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-06-23 16:11:58 +1000

    Update AGP & provide a logging interface (#13)
    Co-authored-by: SessionHero01 <SesshioHero01@getsession.org>

--- library/src/main/cpp/logging.cpp
+++ library/src/main/cpp/logging.cpp
@@ -1,47 +1,40 @@
 #include <jni.h>
-#include <android/log.h>
 #include <string_view>
 #include <functional>
 
 #include "session/logging.hpp"
 #include "session/log_level.h"
 
-#define LOG_TAG "LibSession"
+#include "jni_utils.h"
 
-extern "C" JNIEXPORT void JNICALL
-Java_network_loki_messenger_libsession_1util_util_Logger_initLogger(JNIEnv* env, jclass clazz) {
-    session::add_logger([](std::string_view msg, std::string_view category, session::LogLevel level) {
-        android_LogPriority prio = ANDROID_LOG_VERBOSE;
+using namespace jni_utils;
 
-        switch (level.level) {
-            case LOG_LEVEL_TRACE:
-                prio = ANDROID_LOG_VERBOSE;
-                break;
+extern "C"
+JNIEXPORT void JNICALL
+Java_network_loki_messenger_libsession_1util_util_Logger_00024Companion_addLogger(JNIEnv *env, jclass clazz,
+                                                                   jobject logger) {
+    JavaVM *vm = nullptr;
+    env->GetJavaVM(&vm);
+    if (!vm) {
+        env->ThrowNew(env->FindClass("java/lang/RuntimeException"), "Failed to get JavaVM");
+        return;
+    }
 
-            case LOG_LEVEL_DEBUG:
-                prio = ANDROID_LOG_DEBUG;
-                break;
+    jmethodID logMethod = env->GetMethodID(env->GetObjectClass(logger), "log",
+                                  "(Ljava/lang/String;Ljava/lang/String;I)V");
 
-            case LOG_LEVEL_INFO:
-                prio = ANDROID_LOG_INFO;
-                break;
-
-            case LOG_LEVEL_WARN:
-                prio = ANDROID_LOG_WARN;
-                break;
-
-            case LOG_LEVEL_ERROR:
-            case LOG_LEVEL_CRITICAL:
-                prio = ANDROID_LOG_ERROR;
-                break;
-
-            default:
-                prio = ANDROID_LOG_INFO;
-                break;
+    auto loggerRef = env->NewGlobalRef(logger);
+    session::add_logger([vm, logMethod, loggerRef](std::string_view msg, std::string_view category, session::LogLevel level) {
+        JNIEnv *env = nullptr;
+        vm->AttachCurrentThread(&env, nullptr);
+        if (!env) {
+            return; // Failed to attach thread, cannot log
         }
 
-        __android_log_print(prio, LOG_TAG, "%.*s [%.*s]",
-                    static_cast<int>(msg.size()), msg.data(),
-                    static_cast<int>(category.size()), category.data());
-});
+        env->CallVoidMethod(loggerRef, logMethod,
+                            JavaLocalRef(env, env->NewStringUTF(msg.data())).get(),
+                            JavaLocalRef(env, env->NewStringUTF(category.data())).get(),
+                            static_cast<jint>(level.level)
+        );
+    });
 }
