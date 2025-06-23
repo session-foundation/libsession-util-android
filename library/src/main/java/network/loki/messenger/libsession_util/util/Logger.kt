9ead7ac646dfc78174a388506841f20c820c5510
SessionHero01 <SesshioHero01@getsession.org>
2025-06-23 15:59:56 +1000

    Update AGP & provide a logging interface

--- library/src/main/java/network/loki/messenger/libsession_util/util/Logger.kt
+++ library/src/main/java/network/loki/messenger/libsession_util/util/Logger.kt
@@ -1,11 +1,23 @@
 package network.loki.messenger.libsession_util.util
 
-object Logger {
+typealias LogLevel = Int
 
-    init {
-        System.loadLibrary("session_util")
+interface Logger {
+    fun log(message: String, category: String, level: LogLevel)
+
+    companion object {
+        const val LOG_LEVEL_TRACE: LogLevel = 0
+        const val LOG_LEVEL_DEBUG: LogLevel = 1
+        const val LOG_LEVEL_INFO: LogLevel = 2
+        const val LOG_LEVEL_WARN: LogLevel = 3
+        const val LOG_LEVEL_ERROR: LogLevel = 4
+        const val LOG_LEVEL_CRITICAL: LogLevel = 5
+        const val LOG_LEVEL_OFF: LogLevel = 6
+
+        init {
+            System.loadLibrary("session_util")
+        }
+
+        external fun addLogger(logger: Logger)
     }
-
-    @JvmStatic
-    external fun initLogger()
 }
